4bd7ee1f75d67d0027bce39d04ad7351d01a34f5
Ivan OULE <ivan.oule75@gmail.com>
2023-07-28 13:00:17 +0000

    commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'test-deployment-dev',
+    appId: '1:288067644:web:80fa91bcbaec18e3e2b339',
+    storageBucket: 'test-deployment-dev.appspot.com',
+    apiKey: 'AIzaSyAQPiAF8mp7ycM-jhedBkpInob3guDiOZQ',
+    authDomain: 'test-deployment-dev.firebaseapp.com',
+    messagingSenderId: '288067644',
+  },
+};

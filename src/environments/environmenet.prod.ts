19ff75c1d4917dcaf8b646db93067fcab95a21c2
Ivan OULE <ivan.oule75@gmail.com>
2023-08-01 11:32:30 +0000

    trying on auto express-auto-deployment-app

---
+++ src/environments/environmenet.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+    firebaseConfig: {
+    projectId: 'test-deployment-dev',
+    appId: '1:288067644:web:80fa91bcbaec18e3e2b339',
+    storageBucket: 'test-deployment-dev.appspot.com',
+    apiKey: 'AIzaSyAQPiAF8mp7ycM-jhedBkpInob3guDiOZQ',
+    authDomain: 'test-deployment-dev.firebaseapp.com',
+    messagingSenderId: '288067644',
+  },
+};
